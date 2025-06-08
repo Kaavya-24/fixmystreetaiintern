0c01795cef7478253f3fc1b2803e0bf5b5f78eec
Kaavya S <71762307024@cit.edu.in>
2025-06-08 23:33:04 +0530

    edited

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>FixMyStreetkAI</title>
+        <title>FixMyStreetAI</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
